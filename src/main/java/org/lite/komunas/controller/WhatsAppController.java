0fd02b76e8d398bee54dfb4797e6420a36b103a0
mehmetsen <mehmetsen80@gmail.com>
2025-08-21 22:48:07 -0500

    add the missing RestController annotation

--- src/main/java/org/lite/komunas/controller/WhatsAppController.java
+++ src/main/java/org/lite/komunas/controller/WhatsAppController.java
@@ -9,8 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RestController;
 
-
+@RestController
 @Slf4j
 @RequestMapping("/whatsapp")
 @RequiredArgsConstructor
